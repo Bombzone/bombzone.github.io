e4c48b386790d38f50a1b3b56e385190720818f9
Ryan Posey <ryan.m.posey@gmail.com>
2025-09-13 11:15:44 -0600

    init

---
+++ js/components.js
@@ -0,0 +1,37 @@
+$().ready(() => {
+    fetch('../nav.html')
+        .then(response => response.text())
+        .then((data) => {
+            $(".nav").replaceWith(data);
+        });
+});
+console.log(window.location.pathname);
+
+
+switch (window.location.pathname) {
+    case "/":
+    case "/index.html":
+        $("#home").addClass('active').attr("aria-current", "page");
+        break
+    case "/about.html":
+        $("#about").addClass('active').attr("aria-current", "page");
+
+        break;
+    case "/projects.html":
+        $("#projects").addClass('active').attr("aria-current", "page");
+
+        break;
+    case "/accomplishments.html":
+        $("#accomplishments").addClass('active').attr("aria-current", "page");
+
+        break;
+    case "/resume.html":
+        $("#resume").addClass('active').attr("aria-current", "page");
+
+        break;
+    case "/contact.html":
+
+        break;
+    default:
+        break;
+}
