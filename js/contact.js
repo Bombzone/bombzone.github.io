ebe32b927a213a9cfe0566c0ab02ad4966fff28a
Ryan Posey <ryan.m.posey@gmail.com>
2025-09-15 01:13:19 -0600

    fix?

--- js/contact.js
+++ js/contact.js
@@ -1,48 +1,25 @@
-// scripts.js
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-'use strict'
-    const contactForm = document.getElementById("contact-form")
-    const status = document.getElementById("status")
+const form = document.getElementById('myForm');
 
-    var onSubmit = function (token) {
-        if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-        }
-        var data = new FormData(event.target);
-        fetch(event.target.action, {
-            method: form.method,
-            body: data,
-            headers: {
-                'Accept': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                status.innerHTML = "Thanks for your submission!";
-                form.reset()
-            } else {
-                response.json().then(data => {
-                    if (Object.hasOwn(data, 'errors')) {
-                        status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-                    } else {
-                        status.innerHTML = "Oops! There was a problem submitting your form"
-                    }
-                })
-            }
-        }).catch(error => {
-            status.innerHTML = "Oops! There was a problem submitting your form"
-        });
+form.addEventListener('submit', function (event) {
 
-        form.classList.add('was-validated')
+    if (form.checkValidity()) {
+        form.classList.add('was-validated');
+        // Form is valid, execute reCAPTCHA
+        grecaptcha.execute();
+    } else {
+        event.preventDefault();
+        event.stopPropagation();
 
-    };
+    }
+});
+function onSubmit(token) {
+    // Append the token to the form
+    const tokenInput = document.createElement('input');
+    tokenInput.type = 'hidden';
+    tokenInput.name = 'g-recaptcha-response';
+    tokenInput.value = token;
+    form.appendChild(tokenInput);
 
-    var onloadCallback = function () {
-        grecaptcha.render('submit', {
-            'sitekey': '6Le-8ckrAAAAALzGSEXhIYFFUwtO4NT8K5o6XCks',
-            'callback': onSubmit
-        });
-    };
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    // Loop over them and prevent submission
+    // Optionally, you can now submit the form programmatically (POST or AJAX)
+    form.submit();
+}
