5274e3372ad16345a1736e2351cc7cea40837938
Ryan Posey <ryan.m.posey@gmail.com>
2025-09-15 09:22:23 -0600

    fix validation

--- js/contact.js
+++ js/contact.js
@@ -1,40 +1,25 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
-
 const btn = document.getElementById('button');
 
 document.getElementById('myForm')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+    .addEventListener('submit', function (event) {
+        event.preventDefault()
+        if (!form.checkValidity()) {
+            event.stopPropagation()
+        }
 
-   btn.value = 'Sending...';
+        form.classList.add('was-validated')
 
-   const serviceID = 'default_service';
-   const templateID = 'template_vhaqu4t';
+        btn.value = 'Sending...';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+        const serviceID = 'default_service';
+        const templateID = 'template_vhaqu4t';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
     });
-});
