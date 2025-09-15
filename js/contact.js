4a86d55d6d8558bded60cea9c3a5489775c9a23c
Bombzone <64180489+Bombzone@users.noreply.github.com>
2025-09-15 15:06:00 +0000

    Deploying to gh-pages from @ Bombzone/bombzone.github.io@62a31009c0bf54a83be05bcc5d62ade0d6e393f1 🚀

--- js/contact.js
+++ js/contact.js
@@ -1,24 +1,40 @@
-const form = document.getElementById('myForm');
-const formBtn = document.getElementById("formBtn");
-formBtn.addEventListener('click', function(event) {
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
 
-  if (form.checkValidity()) {
-    form.classList.add('was-validated');
-    // Form is valid, execute reCAPTCHA
-    grecaptcha.execute("6Le-8ckrAAAAALzGSEXhIYFFUwtO4NT8K5o6XCks", onSubmit);
-  } else {
-      event.preventDefault();
-  event.stopPropagation();
-  }
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+const btn = document.getElementById('button');
+
+document.getElementById('myForm')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_vhaqu4t';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
 });
-function onSubmit(token) {
-  // Append the token to the form
-  const tokenInput = document.createElement('input');
-  tokenInput.type = 'hidden';
-  tokenInput.name = 'g-recaptcha-response';
-  tokenInput.value = token;
-  form.appendChild(tokenInput);
-
-  // Optionally, you can now submit the form programmatically (POST or AJAX)
-  form.submit();
-}
