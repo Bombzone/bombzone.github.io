1126736940c137b6105fb8c547412462c9e91fec
Bombzone <64180489+Bombzone@users.noreply.github.com>
2025-09-15 08:00:40 +0000

    Deploying to gh-pages from @ Bombzone/bombzone.github.io@f5dd80697d4b79b9094514714123b335d43eb5ec 🚀

--- js/contact.js
+++ js/contact.js
@@ -1,25 +1,24 @@
 const form = document.getElementById('myForm');
 
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', function(event) {
 
-    if (form.checkValidity()) {
-        form.classList.add('was-validated');
-        // Form is valid, execute reCAPTCHA
-        grecaptcha.execute();
-    } else {
-        event.preventDefault();
-        event.stopPropagation();
-
-    }
+  if (form.checkValidity()) {
+    form.classList.add('was-validated');
+    // Form is valid, execute reCAPTCHA
+    grecaptcha.execute("6Le-8ckrAAAAALzGSEXhIYFFUwtO4NT8K5o6XCks", onSubmit);
+  } else {
+      event.preventDefault();
+  event.stopPropagation();
+  }
 });
 function onSubmit(token) {
-    // Append the token to the form
-    const tokenInput = document.createElement('input');
-    tokenInput.type = 'hidden';
-    tokenInput.name = 'g-recaptcha-response';
-    tokenInput.value = token;
-    form.appendChild(tokenInput);
+  // Append the token to the form
+  const tokenInput = document.createElement('input');
+  tokenInput.type = 'hidden';
+  tokenInput.name = 'g-recaptcha-response';
+  tokenInput.value = token;
+  form.appendChild(tokenInput);
 
-    // Optionally, you can now submit the form programmatically (POST or AJAX)
-    form.submit();
+  // Optionally, you can now submit the form programmatically (POST or AJAX)
+  form.submit();
 }
